0c6c56ce73b104770c4e73febced38cf2cc77066
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-01-18 15:54:42 +0200

    steavengameryt

---
+++ steavengameryt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+flatpak install app/com.chatterino.chatterino/x86_64/stable \
+		app/org.atheme.audacious/x86_64/stable \
+		app/org.virt_manager.virt-manager/x86_64/stable \
+		app/com.moonlight_stream.Moonlight/x86_64/stable \
+		app/org.bleachbit.BleachBit/x86_64/stable \
+		app/org.kde.filelight/x86_64/stable \
+		app/com.brave.Browser/x86_64/stable \
+		app/org.libreoffice.LibreOffice/x86_64/stable \
+		app/org.onlyoffice.desktopeditors/x86_64/stable
