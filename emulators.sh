f402704540455c8127821f852053353fd86a4a6f
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-01-18 20:43:44 +0200

    its sad day but ryujinx is gone from flathub

--- emulators.sh
+++ emulators.sh
@@ -5,7 +5,6 @@ flatpak install app/org.duckstation.DuckStation/x86_64/stable \
 		app/org.DolphinEmu.dolphin-emu/x86_64/stable \
 		app/info.cemu.Cemu/x86_64/stable \
 		app/org.libretro.RetroArch/x86_64/stable \
-		app/org.ryujinx.Ryujinx/x86_64/stable \
 		app/org.ppsspp.PPSSPP/x86_64/stable \
 		app/app.xemu.xemu/x86_64/stable \
 		app/net.kuribo64.melonDS/x86_64/stable \
