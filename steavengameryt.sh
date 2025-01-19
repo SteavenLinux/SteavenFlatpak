ec81ed6fceb8c8a4407a7d0114e15b1a81d0cd89
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-01-19 03:10:32 +0200

    more packages

--- steavengameryt.sh
+++ steavengameryt.sh
@@ -7,4 +7,17 @@ flatpak install app/com.chatterino.chatterino/x86_64/stable \
 		app/org.kde.filelight/x86_64/stable \
 		app/com.brave.Browser/x86_64/stable \
 		app/org.libreoffice.LibreOffice/x86_64/stable \
-		app/org.onlyoffice.desktopeditors/x86_64/stable
+		app/org.onlyoffice.desktopeditors/x86_64/stable \
+		app/io.github.flattool.Warehouse/x86_64/stable \
+		app/org.kde.gwenview/x86_64/stable \
+		app/com.dropbox.Client/x86_64/stable \
+		app/com.saivert.pwvucontrol/x86_64/stable \
+		app/com.github.wwmm.easyeffects/x86_64/stable \
+		app/dev.serebit.Waycheck/x86_64/stable \
+		app/fr.handbrake.ghb/x86_64/stable \
+		app/com.rustdesk.RustDesk/x86_64/stable \
+		app/io.github.peazip.PeaZip/x86_64/stable \
+		app/org.videolan.VLC/x86_64/stable \
+		app/org.filezillaproject.Filezilla/x86_64/stable \
+		app/io.github.jonmagon.kdiskmark/x86_64/stable \
+		app/io.gitlab.news_flash.NewsFlash/x86_64/stable
