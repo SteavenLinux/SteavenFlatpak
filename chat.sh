6c62c075ae44c41c47e1751fca29b41d8570ab09
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-03-03 19:16:05 +0200

    app/com.sindresorhus.Caprine/x86_64/stable

--- chat.sh
+++ chat.sh
@@ -4,4 +4,5 @@ flatpak install app/im.riot.Riot/x86_64/stable \
 		app/dev.vencord.Vesktop/x86_64/stable \
 		app/org.telegram.desktop/x86_64/stable \
 		app/com.ktechpit.whatsie/x86_64/stable \
-		app/io.github.Hexchat/x86_64/stable
+		app/io.github.Hexchat/x86_64/stable \
+		app/com.sindresorhus.Caprine/x86_64/stable
